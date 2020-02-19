6d5f4b5902d4ab79e258bbe461f2dc5c217ce2bc
Yasminehassan26 <61247056+Yasminehassan26@users.noreply.github.com>
2020-02-19 20:40:59 +0200

    Add files via upload

---
+++ src/calc/Calc.java
@@ -0,0 +1,25 @@
+package calc;
+
+public class Calc implements icalculator {
+
+    int z;
+    float k;
+
+    @Override
+    public int add(int x, int y) {
+        z = x + y;
+        return z;
+    }
+
+    @Override
+    public float divide(int x, int y) {
+        k = 0;
+        if(y==0){
+        throw new RuntimeException();
+        }
+   else if (x==0){
+return 0;    } 
+    return (float)x/y;
+    }
+
+   }
---
+++ src/calc/icalculator.java
@@ -0,0 +1,8 @@
+
+package calc;
+public interface icalculator {
+    int add(int x , int y);
+    float divide(int x , int y) throws RuntimeException;  
+   
+    
+}
